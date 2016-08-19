31eb43c2180a2876269c1eb845d6432f1731e833
mateus-etto <mateus.etto@gmail.com>
2016-08-19 16:49:46 -0300

    cleanup of commented codes on BackgroundAnimation

--- Assets/Scripts/BackgroundAnimation.cs
+++ Assets/Scripts/BackgroundAnimation.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class BackgroundAnimation : MonoBehaviour
 {
     public AudioSource audioSource;
-//#if UNITY_EDITOR
     public bool on = true;
-//#endif
 
     float[] spectrum;
     int qSamples = 1024;
@@ -37,10 +35,6 @@ public class BackgroundAnimation : MonoBehaviour
         Color[] cores = particleAnimator.colorAnimation;
         yo = CorDoMeio();
         
-         
-        //print("color: " + color);
-        
-        
         cores[0] = new Color(color, yo, 1-color);
         cores[1] = new Color(color, yo, 1-color);
         cores[2] = new Color(color, yo, 1-color);
@@ -48,7 +42,6 @@ public class BackgroundAnimation : MonoBehaviour
         cores[4] = new Color(color, yo, 1-color);
         particleAnimator.colorAnimation = cores;
 
-        //print(contador);
         contador -= color * Time.deltaTime;
         
         if (contador <= 0)
@@ -59,18 +52,8 @@ public class BackgroundAnimation : MonoBehaviour
 
     public void RecalculaContador()
     {
-        //GameObject.Find("GM").GetComponent<GameMananger>().CriaObjeto(color);
-        //print(color);
-
         contador = Random.Range(minC, maxC);
     }
-
-    /*public void RecalculaContador(float c)
-    {
-        GameObject.Find("GM").GetComponent<GameMananger>().CriaObjeto(c);
-
-        contador = Random.Range(minC, maxC);
-    }*/
 
     public Color DevolveCor()
     {
@@ -79,7 +62,6 @@ public class BackgroundAnimation : MonoBehaviour
 
     float CorDoMeio()
     {
-        //return 1 - Mathf.Abs(color - 1);
         if (color >= 0.5f)
         {
             return 1 - Mathf.Sqrt(color-0.5f);
@@ -98,40 +80,29 @@ public class BackgroundAnimation : MonoBehaviour
 
         for (int i = 0; i < qSamples / lossFactor - 1; i++)
         {
-            sumX += i * lossFactor;// / 2;
+            sumX += i * lossFactor;
             sumY += ((Mathf.Log(spectrum[i * lossFactor]) + 10) * 10);
-            sumX2 += (i * lossFactor /*/ 2*/) * (i * lossFactor /*/ 2*/);
-            sumXY += (i * lossFactor /*/ 2*/) * ((Mathf.Log(spectrum[i * lossFactor]) + 10) * 10);
+            sumX2 += (i * lossFactor) * (i * lossFactor);
+            sumXY += (i * lossFactor) * ((Mathf.Log(spectrum[i * lossFactor]) + 10) * 10);
 
-            sumX += (i * lossFactor) + 1;// / 2;
+            sumX += (i * lossFactor) + 1;
             sumY += ((Mathf.Log(spectrum[(i * lossFactor) + 1]) + 10) * 10);
-            sumX2 += ((i * lossFactor) + 1 /*/ 2*/) * ((i * lossFactor) + 1 /*/ 2*/);
-            sumXY += ((i * lossFactor) + 1 /*/ 2*/) * ((Mathf.Log(spectrum[(i * lossFactor) + 1]) + 10) * 10);
+            sumX2 += ((i * lossFactor) + 1) * ((i * lossFactor) + 1);
+            sumXY += ((i * lossFactor) + 1) * ((Mathf.Log(spectrum[(i * lossFactor) + 1]) + 10) * 10);
         }
 
         //Line adjustment
         a = ((qSamples * 0.5f) * sumXY - sumX * sumY) / ((qSamples * 0.5f) * sumX2 - sumX * sumX);
         b = (sumY - a * sumX) / (qSamples * 0.5f);
 
-//#if UNITY_EDITOR
-        if (on)
-        {
-            //color = (-b / (a * qSamples));  //Base
-            
-            //fim: Min= -0.27; Max= 1.42
-            //color = ((-b / (a * qSamples)) + 0.3f) / 1.72f;// *1.2f;
+        //fim: Min= -0.25; Max= 1.5
+        color = ((-b / (a * qSamples)) + 0.25f) / 1.75f;// *1.2f;
 
-            //Penguin: Min= 0; Max= 1.2
-            //color = ((-b / (a * qSamples)) + 0f) / 1.2f;
-
-            //fim: Min= -0.25; Max= 1.5
-            color = ((-b / (a * qSamples)) + 0.25f) / 1.75f;// *1.2f;
-        }
-        else
-            color = 0;
-//#elif UNITY_WP8
-#if UNITY_WP8
-        color = ((-b / (a * qSamples)) + 0.3f) / 1.72f;// *1.2f;
-#endif
+        // DETAILS AND OTHER ALTERNATIVES
+        //color = (-b / (a * qSamples));  //Base
+        //fim: Min= -0.27; Max= 1.42
+        //color = ((-b / (a * qSamples)) + 0.3f) / 1.72f;// *1.2f;
+        //Penguin: Min= 0; Max= 1.2
+        //color = ((-b / (a * qSamples)) + 0f) / 1.2f;
     }
 }
