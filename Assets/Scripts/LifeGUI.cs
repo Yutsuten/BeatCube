f648a1cba28d79a6efd74a7420afadd9a75fb49f
mateus-etto <mateus.etto@gmail.com>
2016-08-31 18:21:51 -0300

    remove commented code on LifeGUI

--- Assets/Scripts/LifeGUI.cs
+++ Assets/Scripts/LifeGUI.cs
@@ -28,9 +28,6 @@ public class LifeGUI : MonoBehaviour
     public void Desativa()
     {
         ativa = false;
-
-        //GetComponent<GUITexture>().color = new Color(0.3f, 0.3f, 184);
-        //GetComponent<GUITexture>().color = new Color(GetComponent<GUITexture>().color.r, GetComponent<GUITexture>().color.g, GetComponent<GUITexture>().color.b, 70);
     }
 
     public void Ativa()
