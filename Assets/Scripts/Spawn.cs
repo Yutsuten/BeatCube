3d267fcd94af43a395920446d5733b9fcc10c4fd
mateus-etto <mateus.etto@gmail.com>
2016-08-31 19:07:09 -0300

    improved logic at Spawn.cs

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour 
 {
-    public GameObject alvo;
+    public GameObject cube;
     public GameObject item;
 
-    private System.String[] cubeTags = { "Target Azul", "Target Vermelho", "Target Amarelo" };
+    private System.String[] cubeTags = { "Target Azul", "Target Vermelho", "Target Amarelo", "Untagged", "Item" };
 
     private GameMananger gameManager;
 
@@ -23,9 +23,9 @@ public class Spawn : MonoBehaviour
     public void SpawnObject(float direction, float intensity)
     {
         if (Random.Range(0, 100) < 80)
-            CreateCube(direction, intensity);
+            CreateTarget(cube, CubeColor(), direction, intensity);
         else
-            CreateItem(direction, intensity);
+            CreateTarget(item, 5, direction, intensity);
     }
 
     private int CubeColor()
@@ -39,43 +39,20 @@ public class Spawn : MonoBehaviour
             return 3;
     }
 
-    private void CreateCube(float direction, float intensity)
+    private void CreateTarget(GameObject targetType, int cubeColor, float direction, float intensity)
     {
-        int cubeColor = CubeColor(); // Randomly determine the cube color
-
-        GameObject newCube = Instantiate(alvo, new Vector3(Random.Range(-3.4f, 3.4f), 6.2f, -1f), Quaternion.identity) as GameObject;
-        gameManager.AddObjectToList(newCube);
+        GameObject newTarget = Instantiate(targetType, new Vector3(Random.Range(-3.4f, 3.4f), 6.2f, -1f), Quaternion.identity) as GameObject;
+        gameManager.AddObjectToList(newTarget);
 
         // Setting color properties
-        for (int i = 0; i < newCube.transform.childCount; i += 1)
+        for (int i = 0; i < newTarget.transform.childCount; i += 1)
         {
-            newCube.transform.GetChild(i).GetComponent<TargetCubeFragment>().cubeColor = cubeColor; // Attribute the color to child cubes
+            newTarget.transform.GetChild(i).GetComponent<TargetCubeFragment>().cubeColor = cubeColor; // Attribute the color to child cubes
         }
-        newCube.transform.tag = cubeTags[cubeColor - 1]; // Set the tag
+        newTarget.transform.tag = cubeTags[cubeColor - 1]; // Set the tag
 
         // Adding the velocity and instantiate
         //print("Direction: " + direction + "; Intensity: " + intensity);
-        newCube.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(direction, -intensity, 0));
-
-        //obj.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-IntensidadeForca(0, 200, intensidade), IntensidadeForca(0, 200, intensidade)), -IntensidadeForca(100, 200, intensidade), 0));
-
-        //gameObj.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-100, 100), -100, 0)); // <- Edit here!!!
-    }
-
-    public void CreateItem(float direction, float intensity)
-    {
-        GameObject newItem = Instantiate(item, new Vector3(Random.Range(-3.4f, 3.4f), 6.2f, -1f), Quaternion.identity) as GameObject;
-        gameManager.AddObjectToList(newItem);
-
-        // Setting color properties (5, 6 or 7)
-        for (int i = 0; i < newItem.transform.childCount; i += 1)
-        {
-            newItem.transform.GetChild(i).GetComponent<TargetCubeFragment>().cubeColor = 5;
-        }
-        newItem.transform.tag = "Item";
-
-        // Adding the velocity and instantiate
-        //print("Direction: " + direction + "; Intensity: " + intensity);
-        newItem.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(direction, -intensity, 0));
+        newTarget.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(direction, -intensity, 0));
     }
 }
