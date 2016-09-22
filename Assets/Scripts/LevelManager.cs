93d2c21de420c1e42d10be34a0fb5e93ce739b34
mateus-etto <mateus.etto@gmail.com>
2016-09-22 00:41:55 -0300

    more adjusts in game level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -26,21 +26,19 @@ public class LevelManager : MonoBehaviour
         float spawnSpeed;
         // Difficulties
         if (timeNow <= 49) // easy
-        {
             spawnSpeed = initialSpawnSpeed - (timeNow / 490); // spawn from 2.0f to 1.9f
-        }
         else if (timeNow <= 169) // medium
-        {
             spawnSpeed = initialSpawnSpeed - 0.1f * (Mathf.Sqrt(timeNow) - 6f); // spawn from 1.9f to 1.3f
-        }
-        else if (timeNow <= 1122) // hard
-        {
-            spawnSpeed = initialSpawnSpeed - (0.56f + 0.02f * (Mathf.Sqrt(timeNow) - 6f)); // spawn from 1.3f to 1.1f
-        }
-        else // insane
-        {
-            spawnSpeed = 1.05f;
-        }
+        else if (timeNow <= 360) // hard
+            spawnSpeed = 1.15f;
+        else if (timeNow <= 480) // very hard
+            spawnSpeed = 1.00f;
+        else if (timeNow <= 600) // insane
+            spawnSpeed = 0.90f;
+        else if (timeNow <= 720) // wow
+            spawnSpeed = 0.80f;
+        else // impossible
+            spawnSpeed = 0.70f;
         Invoke("NextTarget", spawnSpeed);
     }
 
@@ -51,7 +49,7 @@ public class LevelManager : MonoBehaviour
 
     private float Velocity()
     {
-        float velocity = 60 + 2.0f * Mathf.Sqrt(gameTime.TimeElapsed()) + Random.RandomRange(-5f, 10f);
+        float velocity = 60 + 2.5f * Mathf.Sqrt(3 * gameTime.TimeElapsed()) + Random.RandomRange(-5f, 10f);
         // if velocity gets too big, debuff it a little
         if (velocity > 150)
             velocity = 150 + ((velocity - 150) / 3);
